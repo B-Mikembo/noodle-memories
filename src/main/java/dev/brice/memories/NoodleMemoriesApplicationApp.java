9e1d2fb88bbd9fcc315456de5b7e92c5b6b3d28f
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-09-26 20:13:18 +0200

    Apply module: spring-boot

---
+++ src/main/java/dev/brice/memories/NoodleMemoriesApplicationApp.java
@@ -0,0 +1,23 @@
+package dev.brice.memories;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import dev.brice.memories.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@SpringBootApplication
+@ExcludeFromGeneratedCodeCoverage(reason = "Not testing logs")
+public class NoodleMemoriesApplicationApp {
+
+  private static final Logger log = LoggerFactory.getLogger(NoodleMemoriesApplicationApp.class);
+
+  public static void main(String[] args) {
+    Environment env = SpringApplication.run(NoodleMemoriesApplicationApp.class, args).getEnvironment();
+
+    if (log.isInfoEnabled()) {
+      log.info(ApplicationStartupTraces.of(env));
+    }
+  }
+}

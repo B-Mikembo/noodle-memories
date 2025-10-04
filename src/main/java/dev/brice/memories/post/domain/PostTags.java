9906b0dd0bc878bc7643919ee66e4f59ed5e13e3
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-10-04 21:10:44 +0200

    should get empty post tags

---
+++ src/main/java/dev/brice/memories/post/domain/PostTags.java
@@ -0,0 +1,11 @@
+package dev.brice.memories.post.domain;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class PostTags {
+
+  public Set<PostTag> get() {
+    return Collections.emptySet();
+  }
+}
---
+++ src/test/java/dev/brice/memories/post/domain/PostTagsUnitTest.java
@@ -0,0 +1,13 @@
+package dev.brice.memories.post.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class PostTagsUnitTest {
+
+  @Test
+  void shouldGetEmptyPostTags() {
+    assertThat(new PostTags().get()).isEmpty();
+  }
+}

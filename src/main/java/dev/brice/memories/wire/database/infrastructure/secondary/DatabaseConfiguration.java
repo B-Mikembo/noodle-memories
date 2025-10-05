2144b2755f1bc159fe675d1a435beddf3e2d15fb
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-10-05 22:00:17 +0200

    Apply module: jpa-postgresql

---
+++ src/main/java/dev/brice/memories/wire/database/infrastructure/secondary/DatabaseConfiguration.java
@@ -0,0 +1,10 @@
+package dev.brice.memories.wire.database.infrastructure.secondary;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = { "dev.brice.memories" }, enableDefaultTransactions = false)
+class DatabaseConfiguration {}
